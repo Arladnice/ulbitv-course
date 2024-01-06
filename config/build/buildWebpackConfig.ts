528d6e038171c38fdcdedbf906f8be4225825768
Denis Arlakov <darlakov@mail.ru>
2024-01-06 18:51:14 +0300

    Theme

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import { Configuration as DevServerConfiguration } from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(
+  options: BuildOptions
+): DevServerConfiguration {
+  const { paths, mode, isDev } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
