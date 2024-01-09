7d3f941f563bc731bc7b6cd0bad6f0baad49c8e8
Denis Arlakov <darlakov@mail.ru>
2024-01-10 00:44:39 +0300

    config

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import "./styles/index.scss";
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О Сайте</Link>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import classes from "./Counter.module.scss";
-export const Counter = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => setCounter(counter + 1);
-
-  return <div className={classes.counter} onClick={increment}>Counter is {counter}</div>;
-};
