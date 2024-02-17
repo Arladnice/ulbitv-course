bf38283902885aeff61cb0e29371bbf5ee3ebb6d
Denis Arlakov <darlakov@mail.ru>
2024-02-17 23:39:59 +0300

    errorBoundary

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,22 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={<div className='page-wrapper'>{element}</div>}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+            <Route
+                key={path}
+                path={path}
+                element={
+                    (
+                        <Suspense fallback={<PageLoader />}>
+                            <div className='page-wrapper'>{element}</div>
+                        </Suspense>
+                    )
+                }
+            />
+        ))}
+    </Routes>
 );
